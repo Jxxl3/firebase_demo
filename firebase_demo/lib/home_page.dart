2e09b03ece33b74feb7ee01a9673e2708317de87
Jxxl3 <SmithJJ3@hendrix.edu>
2024-10-07 00:04:28 -0500

    Step 9 & 10

--- firebase_demo/lib/home_page.dart
+++ firebase_demo/lib/home_page.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart'
     hide EmailAuthProvider, PhoneAuthProvider;
 import 'package:flutter/material.dart';
+import 'package:gtk_flutter/yes_no_selection.dart';
 import 'package:provider/provider.dart';
 
 import 'app_state.dart';
@@ -46,12 +47,25 @@ class HomePage extends StatelessWidget {
             builder: (context, appState, _) => Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
+                // Add from here...
+                switch (appState.attendees) {
+                  1 => const Paragraph('1 person going'),
+                  >= 2 => Paragraph('${appState.attendees} people going'),
+                  _ => const Paragraph('No one going'),
+                },
+                // ...to here.
                 if (appState.loggedIn) ...[
+                  // Add from here...
+                  YesNoSelection(
+                    state: appState.attending,
+                    onSelection: (attending) => appState.attending = attending,
+                  ),
+                  // ...to here.
                   const Header('Discussion'),
                   GuestBook(
                     addMessage: (message) =>
                         appState.addMessageToGuestBook(message),
-                    messages: appState.guestBookMessages, // new
+                    messages: appState.guestBookMessages,
                   ),
                 ],
               ],
